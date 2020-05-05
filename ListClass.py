1b5b4226563ac05e93f0956abd2124d17c542233
YutoToguchi <YutoToguchi@LAPTOP-CO8BCU32>
2020-05-05 11:02:33 +0900

    Create ListClass

---
+++ ListClass.py
@@ -0,0 +1,16 @@
+'''
+Created on 2020/05/05
+
+@author: YutoToguchi
+'''
+
+class ListClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
